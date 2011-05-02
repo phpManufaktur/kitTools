d976700f541396ae663d36d36aad4b64e5d1e18d
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-05-02 04:53:47 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitTools/trunk@2 6d932038-f9b6-4f46-bdfd-3a115981cbb0

---
+++ include.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * kitTools
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+/**
+ * kitTools does not include any library automatically,
+ * this will be done by the KeepInTouch (KIT) modules by themselves.
+ * 
+ * So, here is nothing to do...
+ */
+
+?>
---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * kitTools
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+$module_directory     = 'kit_tools';
+$module_name          = 'kitTools';
+$module_function      = 'snippet';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8'; 
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License';
+$module_description   = 'Tools for KeepInTouch (KIT)'; 
+$module_home          = 'http://phpmanufaktur.de/kit_tools';
+$module_guid          = 'C5B8DEF3-0288-4FC8-B584-BFA08B899295';
+
+?>
